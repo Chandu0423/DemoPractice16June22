11e121e3348406ad61efcac010b0a97fc346cc62
chandratechlead <chandra.techlead@gmail>
2022-06-16 17:27:54 +0530

    This is fina file

--- Test.java
+++ Test.java
@@ -1,10 +1,8 @@
 class Test{  
     public static void main(String args[]){  
-<<<<<<< HEAD
+
      System.out.println("Hello Java added by Dev B");
-     System.out.println("This line added by DEV B person");
-=======
      System.out.println("Hello Java added by Dev B and updated by DEV A");  
->>>>>>> 38f92f2932ebaafa94efc847f4d07beee58842be
+
     }  
 } 
